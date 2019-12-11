b83ac122d74d32b5c9c0868d11d5f3a9849186f5
Kalyan Cheerla <32354220+kalyankrishna1729@users.noreply.github.com>
2019-12-11 05:25:29 -0600

    Updating rc to get proper return code of system().

--- systemfn.c
+++ systemfn.c
@@ -11,6 +11,8 @@ int main (int argc, char *argv[])
         return 1;
     snprintf (cmd_buffer, sizeof(cmd_buffer), "%s 2>&1", argv[1]);
     rc = system(cmd_buffer);
-    printf ("rc = %d\n", rc);
+    // system() function return code is present in
+    // upper 8 bits, so doing right shift
+    printf ("rc = %d\n", rc>>8);
     return 0;
 }
