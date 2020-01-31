593133fb8bdc9e5d2fdcc85e1ed937b17979b2d6
Kalyan Cheerla <32354220+kalyankrishna1729@users.noreply.github.com>
2020-01-31 17:38:42 -0600

    Updated the functionality to fetch all replies recursively.

--- python/ParseStashPRComments.py
+++ python/ParseStashPRComments.py
@@ -3,6 +3,13 @@
 import sys
 import json
 
+def replyParser(commentsDict):
+    if bool(commentsDict) is True:
+        for i in range(0, len(commentsDict)):
+            print(" \__ {0}:\n{1}" .format(commentsDict[i]['author']['name'],
+                  commentsDict[i]['text']))
+            replyParser(commentsDict[i]['comments'])
+
 def main(argv):
     if len(argv[1:]) != 1:
         print("{0}: More than 1 arg, exit code {1}" .format(argv[0], 2))
@@ -14,13 +21,15 @@ def main(argv):
 
     for i in range(0, int(jsonDict['size'])):
         if jsonDict['values'][i]['action'] == "COMMENTED":
-            print("{1}: {0}" .format(jsonDict['values'][i]['comment']['text'],
+            try:
+                print("\n{0}" .format(jsonDict['values'][i]['commentAnchor']['path']))
+            except:
+                print("\nNO FILE NAME, PR COMMENTS")
+
+            print("{1}:\n{0}" .format(jsonDict['values'][i]['comment']['text'],
                   jsonDict['values'][i]['comment']['author']['name']))
 
-            for j in range(0, len(jsonDict['values'][i]['comment']['comments'])):
-                print(" \__ {1}: {0}"
-                .format(jsonDict['values'][i]['comment']['comments'][j]['text'],
-                jsonDict['values'][i]['comment']['comments'][j]['author']['name']))
+            replyParser(jsonDict['values'][i]['comment']['comments'])
 
 if __name__ == "__main__":
     main(sys.argv)
