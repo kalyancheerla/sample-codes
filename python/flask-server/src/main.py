5ebc781ad7ab2f14f86cf206b21bf36c77613b2f
kalyan <kalyankrishna1729@gmail.com>
2020-03-12 10:28:54 +0530

    Updating the code to take multiple files for download and also added upload logic too.

--- python/flask-server/src/main.py
+++ python/flask-server/src/main.py
@@ -1,15 +1,48 @@
 #!/bin/env python3
-from flask import Flask, send_file
+import os
+from flask import Flask, send_from_directory, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+
+FILES_PATH = '/usr/src/app/files'
 app = Flask(__name__)
+app.config['FILES_PATH'] = FILES_PATH
 
 @app.route('/')
 def hello_world():
-    return 'Hello Buddy!'
+    return 'Hello World!'
 
-@app.route('/download')
-def download_file():
-	path = "sample.xlsx"
-	return send_file(path, as_attachment=True)
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['FILES_PATH'],
+                               filename)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['FILES_PATH'], filename))
+            return redirect(url_for('download_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
