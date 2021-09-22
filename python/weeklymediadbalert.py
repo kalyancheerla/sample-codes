5d198f4353981678ff40813c0a559ecda46a14e0
Kalyan Ch <32354220+kalyancheerla@users.noreply.github.com>
2021-09-22 22:34:04 +0530

    slight output format changes

--- python/weeklymediadbalert.py
+++ python/weeklymediadbalert.py
@@ -6,12 +6,13 @@ MEDIA_DB = "media.db"
 
 def main():
     db_conn = sqlite3.connect(MEDIA_DB)
-    sql_query = "SELECT id,title,category FROM telugu WHERE " \
+    sql_query = "SELECT id,category,title FROM telugu WHERE " \
                 "pubDate > ?;"
     week_ago = (datetime.date.today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
     records = db_conn.execute(sql_query, (week_ago,))
     for record in records:
-        print(record)
+        data = f"{record[0]}|" + '|'.join(record[1:])
+        print((data[:75] + '..') if len(data) > 75 else data)
 
 if __name__ == "__main__":
     main()
